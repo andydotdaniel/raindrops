d9d19a7d0d32c4c43350459237861667a3b46ee4
Andrew Daniel <andrew.daniel@go-jek.com>
2019-09-21 14:08:12 +0530

    Add tap gesture recognizer for main view.

--- Raindrops/ViewController.swift
+++ Raindrops/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.18, green: 0.16, blue: 0.17, alpha: 1.0)
+        initialSetup()
     }
 
+    private func initialSetup() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onScreenTapped))
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc private func onScreenTapped(gestureRecognizer: UITapGestureRecognizer) {}
 
 }
 
