647629103bc19258c782cff5cd200f24c4aeadb6
Andrew Daniel <andrew.daniel@go-jek.com>
2019-09-21 15:32:08 +0530

    Create tapView that overlays the raindrops.
    When raindrops are added to the main view, taps near or on the created raindrop will not produce new raindrops
    because the view being tapped on is the raindrop instead of the view which has the tap gesture recognizer. This issue
    is solved by creating a tap view that overlays everything making the user only interact with the tap view.

--- Raindrops/ViewController.swift
+++ Raindrops/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let tapView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.18, green: 0.16, blue: 0.17, alpha: 1.0)
@@ -17,14 +23,21 @@ class ViewController: UIViewController {
     }
 
     private func initialSetup() {
+        view.addSubview(tapView)
+        tapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onScreenTapped))
-        view.addGestureRecognizer(tapGestureRecognizer)
+        tapView.addGestureRecognizer(tapGestureRecognizer)
     }
     
     @objc private func onScreenTapped(gestureRecognizer: UITapGestureRecognizer) {
         let tapPosition = gestureRecognizer.location(in: view)
         let raindrop = Raindrop(origin: tapPosition)
         view.addSubview(raindrop)
+        view.sendSubviewToBack(raindrop)
         
         raindrop.animateRipple() { _ in
             raindrop.removeFromSuperview()
